c63e150ea6a0c358a00d35b0e4b3dc4dd90e1b4e
Nupin Pillai <npillai@yodlee.com>
2016-02-15 12:15:08 +0530

    Initial commit

---
+++ app/src/main/java/com/jugaads/hooknotify/NotificationDetailActivity.java
@@ -0,0 +1,45 @@
+package com.jugaads.hooknotify;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class NotificationDetailActivity extends AppCompatActivity {
+
+    private TextView textView = null ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_detail);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+
+        textView = (TextView)findViewById(R.id.textDetailView);
+//        textView.setText(getIntent().getT);
+
+    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//
+//        switch (item.getItemId()) {
+//            case android.R.id.home:
+//                finish();
+//                return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+}
